0c76489deea83bacd98d45aa6126431bd485c15f
fdg513 <123785991+fdg513@users.noreply.github.com>
2025-01-24 11:30:06 +0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 /*
-Copyright © 2022 NianBroken. All rights reserved.
-Github：https://github.com/NianBroken/Firework_Simulator
-Gitee：https://gitee.com/nianbroken/Firework_Simulator
+Copyright © 2022 FXY. All rights reserved.
+Github：https://github.com/fdg513/Firework_Simulator
 本项目采用 Apache-2.0 许可证
 简而言之，你可以自由使用、修改和分享本项目的代码，但前提是在其衍生作品中必须保留原始许可证和版权信息，并且必须以相同的许可证发布所有修改过的代码。
 */
@@ -149,7 +148,7 @@ const store = {
 				? "1.2" //配置文件头默认值(不必是int)
 				: "2", //手机默认
 			wordShell: true, //文字烟花 默认为开启 若不开启可修改为false
-			autoLaunch: true, //自动发射烟花
+			autoLaunch: false, //自动发射烟花
 			finale: true, //同时放更多烟花
 			skyLighting: SKY_LIGHT_NORMAL + "",
 			hideControls: IS_HEADER,
